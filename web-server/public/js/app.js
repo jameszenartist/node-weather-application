96beedd748f860824d07d44c7df321fc22ef939f
James Hansen <jameszenartist@gmail.com>
2022-08-07 21:34:42 -0700

    setup app for Heroku

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,19 +1,3 @@
-// console.log("client side javascript file is loaded!");
-// fetch("http://puzzle.mead.io/puzzle")
-//   .then((response) => response.json())
-//   .then((data) => console.log(data))
-//   .catch((error) => console.log(error));
-// fetch("http://localhost:3000/weather?address=boston")
-//   .then((response) => response.json())
-//   .then((data) => {
-//     if (data.error) {
-//       console.log(data.error);
-//     } else {
-//       console.log(data.location);
-//       console.log(data.forecast);
-//     }
-//   });
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -8,6 +8,8 @@ require("dotenv").config();
 const { geocode } = require("./utils/geocode");
 const { forecast } = require("./utils/forecast");
 const app = express();
+// PORT being set on Heroku:
+const port = process.env.PORT || 3000;
 
 // Define paths for express config
 const publicDirectoryPath = path.join(__dirname, "../public");
@@ -115,6 +117,10 @@ app.get("/*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000");
+// app.listen(3000, () => {
+//   console.log("Server is up on port 3000");
+// });
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
